90eb0a110a8ae7545fb1a489a38d80d6337bb94b
JeffersonG999 <72932666+JeffersonG999@users.noreply.github.com>
2021-11-13 23:06:13 +0100

    Update put-file-sftp.sh

--- put-file-sftp.sh
+++ put-file-sftp.sh
@@ -3,7 +3,7 @@
 for filename in /home/source/file_[0-9]*_*.pdf; do
 if [ -f $filename ]; then
 export SSHPASS='PASSWORD'
-sshpass -e sftp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -oBatchMode=no login@host << EOF
+sshpass -e sftp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -oBatchMode=no LOGIN@HOST << EOF
 cd /home/destination
 put $filename
 bye
